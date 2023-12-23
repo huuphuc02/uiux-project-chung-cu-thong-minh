8f39efb60579613b97c57bd772ddb7ea5caf9c3d
Duy <duyct1234@gmail.com>
2023-12-23 21:08:59 +0700

    created administrator pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import HomePageResident from "./pages/resident/HomePage";
 import HomePageAdmin from "./pages/administrator/HomePage";
 import HomePageManager from "./pages/manager/HomePage";
 import BuildingInfor from "./pages/resident/BuildingInfor";
+import SendNotifications from "./pages/administrator/SendNotifications";
+import Statistics from "./pages/administrator/Statistics";
+import Setting from "./pages/administrator/Setting";
+import ResidentStatistics from "./pages/administrator/ResidentStatistics";
+import SendNotificationsSuccess from "./pages/administrator/SendNotificationsSuccess";
 
 function App() {
   return (
@@ -12,7 +17,12 @@ function App() {
       <Routes>
         <Route path="/homepageResident" element={<HomePageResident />} />
         <Route path="/homepageAdmin" element={<HomePageAdmin />} />
+        <Route path="/sendNotifications" element={<SendNotifications />} />
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/sendNotificationsSuccess" element={<SendNotificationsSuccess />} />
         <Route path="/homepageManager" element={<HomePageManager />} />
+        <Route path="/residentStatistics" element={<ResidentStatistics />} />
         <Route path="/buildingInfo" element={<BuildingInfor />} />
         <Route path="/" element={<Login />} />
       </Routes>
