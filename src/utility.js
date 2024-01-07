06b45f3aefa1aa349b70db70ad471bdfc8fb2f9d
Hữu Phúc <huuphuc608@gmail.com>
2024-01-07 23:32:44 +0700

    post dang ky tam tru tam vang

--- src/utility.js
+++ src/utility.js
@@ -38,3 +38,15 @@ export function generateRandomString(length) {
 
   return result;
 }
+
+export function convertDateFormat(inputDate) {
+  const dateObject = new Date(inputDate);
+
+  const day = String(dateObject.getDate()).padStart(2, "0");
+  const month = String(dateObject.getMonth() + 1).padStart(2, "0");
+  const year = dateObject.getFullYear();
+
+  const formattedDate = `${day}-${month}-${year}`;
+
+  return formattedDate;
+}
